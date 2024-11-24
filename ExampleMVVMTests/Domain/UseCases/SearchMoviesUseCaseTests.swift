9888c0ede9a89136209a802ac0afb5e6bbe85fd6
tomoEng11 <tom_friendship@me.com>
2024-11-24 15:21:57 +0900

    SearchMoviesUseCaseテスト

--- ExampleMVVMTests/Domain/UseCases/SearchMoviesUseCaseTests.swift
+++ ExampleMVVMTests/Domain/UseCases/SearchMoviesUseCaseTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 class SearchMoviesUseCaseTests: XCTestCase {
-    
+
     static let moviesPages: [MoviesPage] = {
         let page1 = MoviesPage(page: 1, totalPages: 2, movies: [
             Movie.stub(id: "1", title: "title1", posterPath: "/1", overview: "overview1"),
@@ -14,44 +14,12 @@ class SearchMoviesUseCaseTests: XCTestCase {
     enum MoviesRepositorySuccessTestError: Error {
         case failedFetching
     }
-    
-    class MoviesQueriesRepositoryMock: MoviesQueriesRepository {
-        var recentQueries: [MovieQuery] = []
-        var fetchCompletionCallsCount = 0
-        
-        func fetchRecentsQueries(
-            maxCount: Int,
-            completion: @escaping (Result<[MovieQuery], Error>) -> Void
-        ) {
-            completion(.success(recentQueries))
-            fetchCompletionCallsCount += 1
-        }
-        func saveRecentQuery(query: MovieQuery, completion: @escaping (Result<MovieQuery, Error>) -> Void) {
-            recentQueries.append(query)
-        }
-    }
-    
-    class MoviesRepositoryMock: MoviesRepository {
-        var result: Result<MoviesPage, Error>
-        var fetchCompletionCallsCount = 0
 
-        init(result: Result<MoviesPage, Error>) {
-            self.result = result
-        }
+    let requestValue = SearchMoviesUseCaseRequestValue(
+        query: MovieQuery(query: "title1"),
+        page: 0
+    )
 
-        func fetchMoviesList(
-            query: MovieQuery,
-            page: Int,
-            cached: @escaping (MoviesPage) -> Void,
-            completion: @escaping (Result<MoviesPage, Error>
-            ) -> Void
-        ) -> Cancellable? {
-            completion(result)
-            fetchCompletionCallsCount += 1
-            return nil
-        }
-    }
-    
     func testSearchMoviesUseCase_whenSuccessfullyFetchesMoviesForQuery_thenQueryIsSavedInRecentQueries() {
         // given
         var useCaseCompletionCallsCount = 0
@@ -65,10 +33,6 @@ class SearchMoviesUseCaseTests: XCTestCase {
         )
 
         // when
-        let requestValue = SearchMoviesUseCaseRequestValue(
-            query: MovieQuery(query: "title1"),
-            page: 0
-        )
         _ = useCase.execute(
             requestValue: requestValue,
             cached: { _ in }
@@ -80,6 +44,7 @@ class SearchMoviesUseCaseTests: XCTestCase {
         moviesQueriesRepository.fetchRecentsQueries(maxCount: 1) { result in
             recents = (try? result.get()) ?? []
         }
+        
         XCTAssertTrue(recents.contains(MovieQuery(query: "title1")))
         XCTAssertEqual(useCaseCompletionCallsCount, 1)
         XCTAssertEqual(moviesQueriesRepository.fetchCompletionCallsCount, 1)
@@ -88,5 +53,24 @@ class SearchMoviesUseCaseTests: XCTestCase {
     
     func testSearchMoviesUseCase_whenFailedFetchingMoviesForQuery_thenQueryIsNotSavedInRecentQueries() {
         //SearchMoviesUseCaseが失敗した場合に、検索クエリが「最近の検索履歴（recentqueries）」に保存されないことを確認するテストを書いてください
+        // 1. MoviesQueriesRepository/MoviesRepositoryのmock
+        let moviesRepositoryMock = MoviesRepositoryMock(
+            result: .failure(MoviesRepositorySuccessTestError.failedFetching)
+        )
+        let moviesQueriesRepositoryMock = MoviesQueriesRepositoryMock()
+
+        let usecase = DefaultSearchMoviesUseCase(moviesRepository: moviesRepositoryMock, moviesQueriesRepository: moviesQueriesRepositoryMock)
+
+        // 2. usecaseのexecute
+        _ = usecase.execute(
+            requestValue: requestValue,
+            cached: { _ in },
+            completion: { _ in }
+        )
+
+        // 3. assert
+        XCTAssertEqual(moviesRepositoryMock.fetchCompletionCallsCount, 1, "fetchMoviesListが呼ばれていないため")
+        XCTAssertEqual(moviesQueriesRepositoryMock.saveCompletionCallsCount, 0, "saveRecentQueryが呼ばれていないため")
+        XCTAssertTrue(moviesQueriesRepositoryMock.recentQueries.isEmpty)
     }
 }
